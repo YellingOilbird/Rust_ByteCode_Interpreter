67d2b5a9bff07f27ebb78f7343244fe2b7da8a4a
YellingOilbird <lysenkori@gmail.com>
2022-04-21 04:40:30 +0300

    Update parser.rs

--- Rust_ByteCode_Interpreter/src/parser.rs
+++ Rust_ByteCode_Interpreter/src/parser.rs
@@ -1,6 +1,7 @@
 use crate::virtual_machine::{Var, Val, Lbl, Operation};
 use std::fs::File;
 use std::io::Read;
+
 use std::{env, collections::HashMap};
 
 #[allow(dead_code)]
@@ -31,21 +32,22 @@ fn parse_val(value: &str) -> Val {
 
 
 #[allow(dead_code)]
-pub(crate) fn parse_code() -> (HashMap<String, u8>, Vec<Operation>) {
+pub(crate) fn parse_code() -> (HashMap<String,u8>, Vec<Operation>) {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
     // create Hashmap for #[modificator] and Vector for operations
     let mut modificators: HashMap<String,u8> = HashMap::default();
     let mut operations: Vec<Operation> = vec![];
+
     // read file to buffer String
     // use this case for virtual_machine.rs tests >>>
-    // let path = filename; <<< 
+    // let path = filename; <<<
     let mut file = File::open(path).unwrap();
     let mut buf = String::new();
     file.read_to_string(&mut buf).unwrap();
 
     // split buffer to instructions
-    let instruction: Vec<&str> = buf.split('\n')
+    let mut instruction: Vec<&str> = buf.split('\n')
                         // split lines
                          .map(|i| i.trim())
                          // clear spaces from lines
@@ -60,54 +62,54 @@ pub(crate) fn parse_code() -> (HashMap<String, u8>, Vec<Operation>) {
         if item.starts_with("#[") && item.ends_with("]") {
             // line number = index of current line - amount of certain modificators
             let label = index  - modificators.len();
-                // add to instructions modificator with label
-                modificators.insert(item[2..item.len() - 1].to_string(), label as Lbl);
-            }
+            // add to instructions modificator with label
+            modificators.insert(item[2..item.len() - 1].to_string(), label as Lbl);
         }
-    // split instructions to blocks for each instruction and drop NaN
-    // match all operations to this block slice and push them into operations map
-    for item in instruction { 
-        let block: Vec<&str> = item.split(|c| c == ' ' || c == ',')
-                                    .filter(|c| c.len() > 0)
-                                    .collect();
-        let operation = match &block[..] {
-            &["LOAD_VAL", value] => Operation::LOAD_VAL(parse_val(value)),
-            &["WRITE_VAR", value] => Operation::WRITE_VAR(parse_var(value)),
-            &["READ_VAR", value] => Operation::READ_VAR(parse_var(value)),
-            &["ADD"] => Operation::ADD,
-            &["MULTIPLY"] => Operation::MULTIPLY,
-            &["INCREMENT"] => Operation::INCREMENT,
-            &["#[BREAK]"] => continue,
-            &["#[LOOP]"] => continue,
-            &["CMP"] => Operation::CMP,
-            // match Operation using labels to modificators map
-            &["JUMP", label] => match modificators.get(label) {
-                Some(label) => Operation::JUMP(*label),
-                None => panic!("label `{}` not found!", label)
-            },
-            &["JUMP_EQUALS", label] => match modificators.get(label) {
-                Some(label) => Operation::JUMP_EQUALS(*label),
-                None => panic!("label `{}` not found!", label)
-            },
-            &["JUMP_GREATER", label] => match modificators.get(label) {
-                Some(label) => Operation::JUMP_GREATER(*label),
-                None => panic!("label `{}` not found!", label)
-            }, 
-            &["JUMP_LESS", label] => match modificators.get(label) {
-                Some(label) => Operation::JUMP_LESS(*label),
-                None => panic!("label `{}` not found!", label)
-            },
-            &["SEND_CHANNEL", data] => Operation::SEND_CHANNEL(str::parse(data).unwrap()),
-            &["RECV_CHANNEL"] => Operation::RECV_CHANNEL,
-            &["SPAWN"] => Operation::SPAWN,
-            &["RETURN_VALUE"] => Operation::RETURN_VALUE,
-            _ => panic!("Wrong Operation")
-        };
-        operations.push(operation);
     }
 
+    //split instructions to blocks for each instruction and drop NaN
+    //match all operations to this block slice and push them into operations map
+        for i in instruction { 
+            let block: Vec<&str> = i.split(|c| c == ' ' || c == ',')
+                                    .filter(|c| c.len() > 0)
+                                    .collect();
+            let operation = match &block[..] {
+                &["LOAD_VAL", value] => Operation::LOAD_VAL(parse_val(value)),
+                &["WRITE_VAR", value] => Operation::WRITE_VAR(parse_var(value)),
+                &["READ_VAR", value] => Operation::READ_VAR(parse_var(value)),
+                &["ADD"] => Operation::ADD,
+                &["MULTIPLY"] => Operation::MULTIPLY,
+                &["INCREMENT"] => Operation::INCREMENT,
+                &["#[BREAK]"] => continue,
+                &["#[LOOP]"] => continue,
+                &["#[FUNC]"] => continue,
+                &["CMP"] => Operation::CMP,
+                // match Operation using labels to modificators map
+                &["JUMP", label] => match modificators.get(label) {
+                    Some(label) => Operation::JUMP(*label),
+                    None => panic!("label `{}` not found!", label)
+                },
+                &["JUMP_EQUALS", label] => match modificators.get(label) {
+                    Some(label) => Operation::JUMP_EQUALS(*label),
+                    None => panic!("label `{}` not found!", label)
+                },
+                &["JUMP_GREATER", label] => match modificators.get(label) {
+                    Some(label) => Operation::JUMP_GREATER(*label),
+                    None => panic!("label `{}` not found!", label)
+                }, 
+                &["JUMP_LESS", label] => match modificators.get(label) {
+                    Some(label) => Operation::JUMP_LESS(*label),
+                    None => panic!("label `{}` not found!", label)
+                },
+                &["SEND_CHANNEL", name] => Operation::SEND_CHANNEL(parse_var(name)),
+                &["RECV_CHANNEL", name] => Operation::RECV_CHANNEL(parse_var(name)),
+                &["SPAWN"] => Operation::SPAWN,
+                &["RETURN_VALUE"] => Operation::RETURN_VALUE,
+                _ => panic!("Wrong Operation")
+            };
+            operations.push(operation);
+        }
     (modificators, operations)
-   
 }
 
 #[test]
@@ -142,4 +144,8 @@ fn test_parse_operations() {
     //let (_, operations) = parse_code("./code_examples/loop1.language");
     //assert_eq!(operations, _operations_vec);
 
+}
+#[test]
+fn test_parse_functions() {
+    parse_code();
 }
